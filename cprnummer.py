cbfceaa49ebf68b918dbc6a280d5de493ed0d77f
Cherry <56732512+Cherry-theCSStudent@users.noreply.github.com>
2022-10-13 11:18:43 +0630

    CPR Number
    https://open.kattis.com/problems/cprnummer

---
+++ cprnummer.py
@@ -0,0 +1,6 @@
+num = input()
+total = int(num[0])*4+ int(num[1])*3+ int(num[2])*2+ int(num[3])*7+ int(num[4])*6+ int(num[5])*5+ int(num[7])*4+ int(num[8])*3+int(num[9])*2+int(num[10])*1
+if(total%11 ==0):
+    print(1)
+else:
+    print(0)
