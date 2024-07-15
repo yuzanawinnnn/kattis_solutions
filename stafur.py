bce09111024f1bd0b854af9bd3471db2e15d988a
Yuzana <56732512+yuzanawinnnn@users.noreply.github.com>
2024-07-16 00:25:56 +0900

    Create stafur.py

---
+++ stafur.py
@@ -0,0 +1,9 @@
+letter = input()
+letter = letter.lower()
+vowel = ['a', 'e', 'i', 'o', 'u']
+if letter in vowel:
+    print('Jebb')
+elif letter == 'y':
+    print('Kannski')
+else:
+    print('Neibb')
