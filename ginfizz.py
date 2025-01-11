fefde7a8e3dcd27fa35d42e3682574f412a6590d
Yuzana <56732512+yuzanawinnnn@users.noreply.github.com>
2025-01-12 00:56:59 +0900

    Create ginfizz.py

---
+++ ginfizz.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+print(45*n,"ml gin")
+print(30*n,"ml fresh lemon juice")
+print(10*n,"ml simple syrup")
+
+if(n > 1):
+    print(1*n,"slices of lemon")
+else:
+    print(1*n,"slice of lemon")
