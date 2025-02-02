440f32db66ddf6dbac8ea7999f423339b8219f89
Yuzana <56732512+yuzanawinnnn@users.noreply.github.com>
2025-02-03 03:45:12 +0900

    Create hvertskalmaeta.py

---
+++ hvertskalmaeta.py
@@ -0,0 +1,5 @@
+s = input()
+
+cities = {"Reykjavik":"Reykjavik", "Kopavogur":"Reykjavik", "Hafnarfjordur":"Reykjavik", "Reykjanesbaer":"Reykjavik", "Akureyri":"Akureyri", "Gardabaer":"Reykjavik", "Mosfellsbaer":"Reykjavik", "Arborg":"Reykjavik", "Akranes":"Reykjavik", "Fjardabyggd":"Akureyri", "Mulathing":"Akureyri", "Seltjarnarnes":"Reykjavik"}
+
+print(cities[s])
